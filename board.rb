afce8a6a4e01c11e6ff543b00875c4c70c9e5069
J Resilient <j.resilient.1@gmail.com>
2019-10-13 14:57:54 -0400

    Add #move_piece

--- board.rb
+++ board.rb
@@ -18,32 +18,46 @@ class Board
 
     def pieces
         (0..1).each do |row_idx|
-            (0..rows.length).each do |col_idx|
+            (0...rows.length).each do |col_idx|
                 rows[row_idx][col_idx] = Piece.new(:black, self, [row_idx, col_idx])
             end
         end
 
         (6..7).each do |row_idx|
-            (0..rows.length).each do |col_idx|
+            (0...rows.length).each do |col_idx|
                 rows[row_idx][col_idx] = Piece.new(:white, self, [row_idx, col_idx])
             end
         end
 
+        pretty_print_board
+    end
+
+    def pretty_print_board
         rows.each do |row| 
             row.each do |square| 
                 print square.to_s unless square.nil?
-                print square if square.nil?
+                print "_____" if square.nil?
                 print "  "
             end
             puts
         end
+        print "\n\n\n"
     end
 
-    # def move_piece(start_pos, end_pos)
-    #     start_row, start_col = start_pos
-    #     end_row, end_col = end_pos
+    def move_piece(start_pos, end_pos)
+        start_row, start_col = start_pos
+        end_row, end_col = end_pos
 
+        raise "There is no piece at #{start_pos}." if rows[start_row][start_col].nil?
+        raise "Cannot move pieces off of board." unless valid_pos?(end_pos) && valid_pos?(start_pos)
 
-    # end
+        rows[end_row][end_col], rows[start_row][start_col] = rows[start_row][start_col], rows[end_row][end_col]
+        pretty_print_board
+    end
+
+    def valid_pos?(pos)
+        pos.all? { |n| n.between?(0,7)}
+    end
 end
 x = Board.new
+x.move_piece([1,7], [2,9])

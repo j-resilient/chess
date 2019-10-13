cf0cac7130ac15cf338e9909887eee2aeff8363a
J Resilient <j.resilient.1@gmail.com>
2019-10-13 13:46:31 -0400

    Add basic board methods

--- board.rb
+++ board.rb
@@ -1,3 +1,15 @@
+require_relative 'piece'
 class Board
+    def [](pos)
+        row, col = pos
+        rows[row][col]
+    end
+
+    def []=(pos, val)
+        rows[pos] = val
+    end
     
+    def initialize
+        @rows = Array.new(8) { Array.new(8) }
+    end
 end

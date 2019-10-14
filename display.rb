47dfa44c2ca1400789db943ca45c4fe0524b4e6c
J Resilient <j.resilient.1@gmail.com>
2019-10-14 18:27:20 -0400

    Add cursor functionality

--- display.rb
+++ display.rb
@@ -1,11 +1,13 @@
 require_relative 'board.rb'
+require_relative 'cursor.rb'
 require 'colorize'
 
 class Display
-    attr_reader :board
+    attr_reader :board, :cursor
 
     def initialize(board)
         @board = board
+        @cursor = Cursor.new([0,0],board)
     end
 
     def render
@@ -15,15 +17,27 @@ class Display
         puts
         board.rows.each_with_index do |row, r_idx| 
             print "#{r_idx}  "
-            row.each do |square|
-                print square.to_s
+            row.each_with_index do |square, c_idx|
+                background = cursor.cursor_pos == [r_idx, c_idx] ? :light_green : :default
+                background = :magenta if background == :light_green && cursor.selected
+
+                print square.to_s.colorize(:background => background)
             end
             puts
         end
         print "\n\n"
     end
+
+    # def run
+    #     x = 0
+    #     until x == 5
+    #         render
+    #         cursor.get_input
+    #         x += 1
+    #     end
+    # end
 end
 
 board = Board.new
 display = Display.new(board)
-display.render
+display.run

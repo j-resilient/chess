beade0ecac61a73daab7b5e080b566e88423992b
J Resilient <j.resilient.1@gmail.com>
2019-10-15 13:52:22 -0400

    Add #make_move

--- human.rb
+++ human.rb
@@ -9,9 +9,9 @@ class HumanPlayer
     def make_move(board)
         pos = []
         until pos.length == 2
-            @display.render
             input = @display.cursor.get_input
             pos << input if input.is_a?(Array)
+            @display.render
         end
         board.move_piece(pos[0], pos[1])
     end
